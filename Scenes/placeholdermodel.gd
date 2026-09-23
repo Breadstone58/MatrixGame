cb8efec468df9613b53759d1897804b031c086d6
Breadstone58 <141655007+Breadstone58@users.noreply.github.com>
2026-09-23 17:17:48 -0400

    stuff added

--- Scenes/placeholdermodel.gd
+++ Scenes/placeholdermodel.gd
@@ -11,12 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta """
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir := Input.get_vector("move_right", "move_left", "move_back", "move_forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -26,3 +26,7 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	if abs(velocity.x) > 0 or abs(velocity.y) > 0 or abs(velocity.z) > 0:
+		$AnimationPlayer.play("root|E1WalkPose")
+	else:
+		$AnimationPlayer.play("root|E1IdlePose")
